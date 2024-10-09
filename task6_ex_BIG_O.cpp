e40aa4371035ee5d1e87ce70ea3e2338e3a9895c
Aleksey <rusal@bk.ru>
2024-10-09 11:22:40 +0300

    Добавьте файлы проекта.

---
+++ task6_ex_BIG_O.cpp
@@ -0,0 +1,22 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+    int nop = 0;
+    int n = 0;
+    for(int z = 2;z<=200; z++){
+        nop = 0;
+        n = z;
+        for (int i = 1; i <= n / 2; i++)
+        {
+            for (int j = 1; j <= n; j += 4) {
+                for (int k = 1; k <= j; k++) {
+                    nop++;
+                }
+            }
+        }
+        cout << nop << endl;
+    }
+}
+
